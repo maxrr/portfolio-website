d6a8326b5a5f2367e52708473516b7db362903d8
Max Rountree <max.r.rountree@gmail.com>
2023-10-19 10:46:30 -0500

    Fixed CLS with button element, switched to div

--- src/lib/components/ProjectCard.svelte
+++ src/lib/components/ProjectCard.svelte
@@ -49,7 +49,8 @@
 				{#if project.links && project.links.length >= 1}
 					<!-- {#each project.links as link} -->
 					<a href={project.links[0].url} target="_blank">
-						<button on:click|stopPropagation>
+						<!-- TODO: Implement on:keypress (A11y suggestion) -->
+						<div class="project-btn" on:click|stopPropagation on:keypress={() => {}} role="button" tabindex="0" aria-label="Project link button to {project.links[0].site}">
 							{#if project.links[0].site == 'github'}<svg
 									xmlns="http://www.w3.org/2000/svg"
 									width="20"
@@ -59,7 +60,8 @@
 									><path
 										d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
 									/></svg
-								>{/if}{project.links[0].text ?? `View on ${project.links[0].site}`}</button
+								>{/if}{project.links[0].text ?? `View on ${project.links[0].site}`}
+							</div
 						>
 					</a>
 					<!-- {/each} -->
@@ -112,7 +114,7 @@
 		object-fit: cover;
 	}
 
-	a > button {
+	.project-btn {
 		@apply btn;
 		@apply variant-glass-surface;
 		@apply py-1;
@@ -123,7 +125,7 @@
 		@apply left-4;
 	}
 
-	a > button:hover {
+	.project-btn:hover {
 		@apply variant-soft-surface;
 	}
 
@@ -147,6 +149,9 @@
 		min-width: 18rem;
 		transform: translateZ(0rem) scale(1);
 		backface-visibility: hidden;
+		perspective: 1000;
+		-webkit-backface-visibility: hidden;
+		-webkit-perspective: 1000;
 		-webkit-font-smoothing: subpixel-antialiased;
 		/* z-index: 1; */
 	}
